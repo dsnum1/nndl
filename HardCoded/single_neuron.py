f7f876c76054423bd0efbb66e9f45121c7e0282c
dsnum1 <dsnum1@gmail.com>
2023-09-29 20:45:15 +0800

    Revert "added git ignore"
    This reverts commit 54254ea5501d27a6f6e26c6a3946df6a6c0819dc.

--- HardCoded/single_neuron.py
+++ HardCoded/single_neuron.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-from plot_architecture import plot_single_neuron
+
 
 class Neuron:
     def __init__(self, w, b):
@@ -21,12 +21,9 @@ class Neuron:
     
     def activation_function(self, u):
         return u
-    
-    def linear_combination(self, x):
-        return self.dot_product(x, self.w)+self.b
 
     def forward(self, x):
-        u = self.linear_combination(x)
+        u = self.dot_product(x, self.w)+self.b
         y = self.activation_function(u)
         return y
     
@@ -46,6 +43,3 @@ if __name__ == '__main__':
 
     # create a forward
     print(n.forward([1,2,3]))
-
-    plot_single_neuron(n, [1,2,3])
-
